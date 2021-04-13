e32278da81e0c728cc9b8db5893f70545557fe23
Pavle Prica <pavle.prica5@gmail.com>
2021-04-13 14:48:11 +0200

    Add custom based time cache impl

---
+++ src/main/kotlin/com/pavleprica/kotlin/cache/time/based/CustomTimeBasedCache.kt
@@ -0,0 +1,68 @@
+package com.pavleprica.kotlin.cache.time.based
+
+import java.util.*
+import java.util.concurrent.ConcurrentHashMap
+
+class CustomTimeBasedCache<T, E> (
+    private var defaultExpiration: Long
+        ): TimeBasedCache<T, E> {
+
+    private val cacheValueMap: ConcurrentHashMap<T, E> = ConcurrentHashMap()
+    private val cacheExpirationMap: ConcurrentHashMap<T, Long> = ConcurrentHashMap()
+
+    override val size: Int
+        get() = cacheValueMap.size
+
+    override val defaultExpirationTime: Long
+        get() = defaultExpiration
+
+    override fun set(key: T, value: E) {
+        cacheExpirationMap[key] = System.currentTimeMillis() + defaultExpiration
+        cacheValueMap[key] = value
+    }
+
+    override fun get(key: T): Optional<E> {
+        expirationTimeCheckAndClean()
+
+        val value = cacheValueMap[key]
+
+        return if (value != null) Optional.of(value) else Optional.empty()
+    }
+
+    private fun expirationTimeCheckAndClean() {
+        val removeKeyList = mutableListOf<T>()
+        cacheExpirationMap.forEach { (key, time) ->
+            if (System.currentTimeMillis() > time) { cacheValueMap.remove(key); removeKeyList.add(key) }
+        }
+        removeKeyList.forEach { cacheExpirationMap.remove(it) }
+    }
+
+    override fun remove(key: T) {
+        cacheValueMap.remove(key)
+        cacheExpirationMap.remove(key)
+    }
+
+    override fun clear() {
+        cacheValueMap.clear()
+        cacheExpirationMap.clear()
+    }
+
+    override fun isEmpty(): Boolean {
+        return cacheValueMap.isEmpty()
+    }
+
+    override fun setDefaultExpirationTime(expirationTime: Long) {
+        require(expirationTime >= 0)
+
+        defaultExpiration = expirationTime
+    }
+
+    override fun set(key: T, value: CustomTimeBasedValue<E>) {
+        require(value.expirationTime >= 0)
+
+        cacheExpirationMap[key] = System.currentTimeMillis() + value.expirationTime
+        cacheValueMap[key] = value.value
+    }
+}
+
+inline fun <reified T, reified E> customTimeBasedCache(expirationTime: Long): CustomTimeBasedCache<T, E> = CustomTimeBasedCache(expirationTime)
