413c09b7cc245ef70a1523ea4e60e769f005be20
Pavle Prica <pavle.prica5@gmail.com>
2021-04-13 12:24:00 +0200

    Add base test for project load

---
+++ src/test/kotlin/com/pavleprica/kotlin/cache/BaseTest.kt
@@ -0,0 +1,18 @@
+package com.pavleprica.kotlin.cache
+
+import io.kotest.core.spec.style.FunSpec
+import org.mockito.Mockito
+
+inline fun <reified T : Any> mock(): T = Mockito.mock(T::class.java)
+
+class BaseTest: FunSpec() {
+
+    init {
+        context("Test load") {
+            test("Test loaded") {
+                println("Success")
+            }
+        }
+    }
+
+}
