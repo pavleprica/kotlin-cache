551f6b52141912493013466971e4c15431701c39
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2022-03-21 13:51:42 +0100

    KACHE-3 Nullable cache keys should probably be allowed after all.

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/time/based/ShortTimeBasedCache.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/time/based/ShortTimeBasedCache.kt
@@ -3,8 +3,8 @@ package io.github.pavleprica.kotlin.cache.time.based
 /**
  * Short time based cache that will hold the cache for 1 minute
  */
-class ShortTimeBasedCache<T: Any, E: Any>: CustomTimeBasedCache<T, E>(ONE_MINUTE) {
+class ShortTimeBasedCache<T, E: Any>: CustomTimeBasedCache<T, E>(ONE_MINUTE) {
 
 }
 
-inline fun <reified T: Any, reified E: Any> shortTimeBasedCache(): ShortTimeBasedCache<T, E> = ShortTimeBasedCache()
+inline fun <reified T, reified E: Any> shortTimeBasedCache(): ShortTimeBasedCache<T, E> = ShortTimeBasedCache()
