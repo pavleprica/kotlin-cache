d2061f0120e2b3f19901220c9c43d192126a3a2e
Tim van der Leeuw <tim.vanderleeuw@hiber.global>
2022-03-21 13:10:31 +0100

    KACHE-3 Kotlin compiler warns that some types need an upper bound on `Any` to ensure they are not nullable; otherwise there will be errors in future Kotlin versions.

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/time/based/ShortTimeBasedCache.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/time/based/ShortTimeBasedCache.kt
@@ -3,8 +3,8 @@ package io.github.pavleprica.kotlin.cache.time.based
 /**
  * Short time based cache that will hold the cache for 1 minute
  */
-class ShortTimeBasedCache<T, E>: CustomTimeBasedCache<T, E>(ONE_MINUTE) {
+class ShortTimeBasedCache<T: Any, E: Any>: CustomTimeBasedCache<T, E>(ONE_MINUTE) {
 
 }
 
-inline fun <reified T, reified E> shortTimeBasedCache(): ShortTimeBasedCache<T, E> = ShortTimeBasedCache()
+inline fun <reified T: Any, reified E: Any> shortTimeBasedCache(): ShortTimeBasedCache<T, E> = ShortTimeBasedCache()
