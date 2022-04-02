26cd2ff9f2e551cf493b47bbe8e54b67dda78386
Pavle Prica <pavle.prica5@gmail.com>
2022-04-02 21:01:29 +0200

    Fix typo

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/model/KeyTimeWrapper.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/model/KeyTimeWrapper.kt
@@ -3,7 +3,7 @@ package io.github.pavleprica.kotlin.cache.model
 import java.time.Duration
 
 /**
- * [KeyTimeWrapper] is used to store timestamps of the expiration time. Using it it will sort them
+ * [KeyTimeWrapper] is used to store timestamps of the expiration time. Using it will sort them
  * with the time given.
  */
 data class KeyTimeWrapper<T>(
