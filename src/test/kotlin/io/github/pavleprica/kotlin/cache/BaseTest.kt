b0f62fdd9f3b01ac65840d73b9864acefabf18a0
Pavle Prica <pavle.prica5@gmail.com>
2022-04-03 21:37:12 +0200

    Refactor mock methods

--- src/test/kotlin/io/github/pavleprica/kotlin/cache/BaseTest.kt
+++ src/test/kotlin/io/github/pavleprica/kotlin/cache/BaseTest.kt
@@ -2,10 +2,11 @@ package io.github.pavleprica.kotlin.cache
 
 import io.kotest.core.spec.style.FunSpec
 import org.mockito.Mockito
+import kotlin.random.Random
 
 inline fun <reified T : Any> mock(): T = Mockito.mock(T::class.java)
 
-class BaseTest: FunSpec() {
+open class BaseTest: FunSpec() {
 
     init {
         context("Test load") {
@@ -14,5 +15,13 @@ class BaseTest: FunSpec() {
             }
         }
     }
+
+    protected val createMockList: (listSize: Int) -> List<Pair<Int, Int>> = { listSize ->
+        val list = mutableListOf<Pair<Int, Int>>()
+        repeat(listSize) { list.add(Pair(it, it)) }
+        list
+    }
+
+    protected val createMockItem: () -> Pair<Int, Int> = { Pair(Random(5).nextInt(), Random(5).nextInt()) }
 
 }
