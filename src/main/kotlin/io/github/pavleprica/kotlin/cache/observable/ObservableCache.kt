99e37240eb916d5d33fe5a063cea709849e2b858
Pavle Prica <pavle.prica5@gmail.com>
2022-04-03 21:51:49 +0200

    Update ObservableCache

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
@@ -2,8 +2,7 @@ package io.github.pavleprica.kotlin.cache.observable
 
 import io.github.pavleprica.kotlin.cache.permanent.PermanentCache
 
-
-class ObservableCache<T, E: Any> : PermanentCache<T, E>() {
+class ObservableCache<T, E : Any> : PermanentCache<T, E>() {
 
     fun subscribeOnAll(callback: () -> Unit) {
         TODO("Not implemented yet")
@@ -28,5 +27,6 @@ class ObservableCache<T, E: Any> : PermanentCache<T, E>() {
     fun subscribeOnDelete(callback: (E) -> Unit) {
         TODO("Not implemented yet")
     }
-
 }
+
+inline fun <reified T, reified E : Any> observableCache(): ObservableCache<T, E> = ObservableCache()
