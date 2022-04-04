4219f42eceec7da6433db10da69fd08c7b7dce2a
Pavle Prica <pavle.prica5@gmail.com>
2022-04-04 21:14:41 +0200

    Add docs

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
@@ -2,14 +2,31 @@ package io.github.pavleprica.kotlin.cache.observable
 
 import io.github.pavleprica.kotlin.cache.permanent.PermanentCache
 
+/**
+ * Observable cache is used if we want to be notified on changes happening inside the cache.
+ * The changes to be notified on are adding and removing values from the cache.
+ */
 class ObservableCache<T, E : Any> : PermanentCache<T, E>() {
 
     private val cacheObservers = mutableListOf<Observer<E>>()
 
+    /**
+     * Subscribes a [Observer] to changes done on cache.
+     * The update method is being invoked on adding and deleting values.
+     *
+     * @param observer to be notified on add or remove.
+     * @see [Observer]
+     */
     fun subscribe(observer: Observer<E>) {
         cacheObservers.add(observer)
     }
 
+    /**
+     * Unsubscribes a [Observer] to changes done on cache.
+     *
+     * @param observer to be unsubscribed from changes.
+     * @see [Observer]
+     */
     fun unSubscribe(observer: Observer<E>) {
         cacheObservers.remove(observer)
     }
