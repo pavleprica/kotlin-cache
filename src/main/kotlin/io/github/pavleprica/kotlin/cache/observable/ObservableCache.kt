7beb168a30fb50d4f94477ffcc20e6f1c7682dc5
Pavle Prica <pavle.prica5@gmail.com>
2022-04-04 17:44:46 +0200

    Base setup of tests

--- src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
+++ src/main/kotlin/io/github/pavleprica/kotlin/cache/observable/ObservableCache.kt
@@ -4,28 +4,16 @@ import io.github.pavleprica.kotlin.cache.permanent.PermanentCache
 
 class ObservableCache<T, E : Any> : PermanentCache<T, E>() {
 
-    fun subscribeOnAll(callback: () -> Unit) {
-        TODO("Not implemented yet")
+    private val cacheObservers = mutableListOf<Observer<E>>()
+
+    fun subscribe(observer: Observer<E>) {
+//        cacheObservers.add(observer)
+        TODO("Not yet implemented")
     }
 
-    fun subscribeOnAll(callback: (E) -> Unit) {
-        TODO("Not implemented yet")
-    }
-
-    fun subscribeOnAdd(callback: () -> Unit) {
-        TODO("Not implemented yet")
-    }
-
-    fun subscribeOnAdd(callback: (E) -> Unit) {
-        TODO("Not implemented yet")
-    }
-
-    fun subscribeOnDelete(callback: () -> Unit) {
-        TODO("Not implemented yet")
-    }
-
-    fun subscribeOnDelete(callback: (E) -> Unit) {
-        TODO("Not implemented yet")
+    fun unSubscribe(observer: Observer<E>) {
+//        cacheObservers.remove(observer)
+        TODO("Not yet implemented")
     }
 }
 
